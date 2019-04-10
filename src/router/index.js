00de7e6a7f395ceb0bf001e1f0c5913696cba6f5
秦帆 <qinfan@qinfandeMac-mini.local>
2019-04-10 17:32:02 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {createAppContainer,createBottomTabNavigator,createStackNavigator} from 'react-navigation'
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../view/home'
 import List from '../view/list'
 import Detail from '../view/detail'
@@ -20,6 +20,18 @@ let Tab = createBottomTabNavigator({
         },
     }
 }, {
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let iconName;
+            if (routeName === 'Home') {
+                iconName = 'ios-home';
+            } else if (routeName === 'List') {
+                iconName = `ios-list`;
+            }
+            return <Ionicons name={iconName} size={25} color={tintColor} />;
+        },
+    }),
     animationEnabled: false,
     swipeEnabled: false,
     tabBarOptions: {
